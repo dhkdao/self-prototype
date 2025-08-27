07972fd5865325e64a9de7aabebfeaed7349ce62
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-27 18:28:05 +0800

    updated

--- packages/web/src/components/Web3Provider.tsx
+++ packages/web/src/components/Web3Provider.tsx
@@ -5,29 +5,29 @@ import { celoAlfajores } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 
-const config = createConfig(
-  getDefaultConfig({
-    // Your dApps chains
-    chains: [celoAlfajores],
-    transports: {
-      // RPC URL for each chain
-      [celoAlfajores.id]: webSocket(
-        `https://celo-alfajores.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
-      ),
-    },
+export const ckConfig = getDefaultConfig({
+  // Your dApps chains
+  chains: [celoAlfajores],
+  transports: {
+    // RPC URL for each chain
+    [celoAlfajores.id]: webSocket(
+      `https://celo-alfajores.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_ID}`,
+    ),
+  },
 
-    // Required API Keys
-    walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+  // Required API Keys
+  walletConnectProjectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
 
-    // Required App Info
-    appName: "DHK dao self-prototype",
+  // Required App Info
+  appName: "DHK dao self-prototype",
 
-    // Optional App Info
-    appDescription: "DHK dao self-prototype",
-    appUrl: "https://dhkdao-self.vercel.app", // your app's url
-    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
-  }),
-);
+  // Optional App Info
+  appDescription: "DHK dao self-prototype",
+  appUrl: "https://dhkdao-self.vercel.app", // your app's url
+  appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+});
+
+const config = createConfig(ckConfig);
 
 const queryClient = new QueryClient();
 
