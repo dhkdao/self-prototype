4c69cf01c2e73cb20049a8ce7dabeb26db6cd5dd
Jimmy Chu <898091+jimmychu0807@users.noreply.github.com>
2025-08-30 19:09:32 +0800

    updated

---
+++ packages/web/src/app/result/page.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import type { VerificationResult } from "@/types";
+import {
+  alfajoresTxUrlPrefix,
+  thresholdTokenAmt,
+  opMainnetAddrUrlPrefix,
+} from "@/config";
+
+export default function VerificationResultPage() {
+  const linkClasses =
+    "mt-4 bg-transparent text-blue-600 underline hover:text-blue-800 hover:underline p-0 border-0 font-normal";
+
+  const [verificationResult, setVerificationResult] =
+    useState<VerificationResult>();
+
+  useEffect(() => {
+    const _verificationResult = JSON.parse(
+      localStorage.getItem("verificationResult") || "",
+    ) as VerificationResult;
+    setVerificationResult(_verificationResult);
+  }, []);
+
+  return (
+    <div className="verification-result-container flex flex-col items-center">
+      <h1 className="text-lg/8 font-medium">DHK dao Self Prototype</h1>
+
+      <section className="flex flex-col items-center my-4">
+        <h1 className="text-base/8">Result</h1>
+        <ul>
+          <li>
+            User address:{" "}
+            {verificationResult?.sender && (
+              <strong>
+                <a
+                  className={linkClasses}
+                  href={opMainnetAddrUrlPrefix + verificationResult?.sender}
+                  target="_blank"
+                >
+                  {verificationResult?.sender}
+                </a>
+              </strong>
+            )}
+          </li>
+          <li>
+            User is self.xyz verified?{" "}
+            {verificationResult?.txHash ? (
+              <strong>
+                <a
+                  className={linkClasses}
+                  href={alfajoresTxUrlPrefix + verificationResult?.txHash}
+                  target="_blank"
+                >
+                  true
+                </a>
+              </strong>
+            ) : (
+              <strong>false</strong>
+            )}
+          </li>
+          <li>
+            User has threshold amount of DHK token (
+            {thresholdTokenAmt.toString()})?{" "}
+            <strong>
+              {verificationResult?.aboveTokenThreshold ? "true" : "false"}
+            </strong>
+          </li>
+          <li>
+            User has DHK ID Subname (mocked)?{" "}
+            <strong>{verificationResult?.hasSubname ? "true" : "false"}</strong>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+}
--- packages/web/src/components/VerificationComponent.tsx
+++ packages/web/src/components/VerificationComponent.tsx
@@ -3,13 +3,14 @@
 import React, { useState, useEffect } from "react";
 import { getUniversalLink } from "@selfxyz/core";
 import { ConnectKitButton } from "connectkit";
+import { useRouter } from "next/navigation";
 import {
   SelfAppBuilder,
   SelfQRcodeWrapper,
   type SelfApp,
 } from "@selfxyz/qrcode";
 import { clsx } from "clsx";
-import type { Log, Hash } from "viem";
+import type { Log } from "viem";
 import { useAccount, useConfig, useWatchContractEvent } from "wagmi";
 import { watchContractEvent } from "wagmi/actions";
 import {
@@ -22,21 +23,12 @@ import {
 } from "@headlessui/react";
 
 import { appName, selfVerificationContract } from "@/config";
-import type { Address } from "viem";
-
-type VerificationCompletedEventArgs = {
-  txHash: Hash;
-  sender: Address;
-  nationality: string;
-  times: number;
-  userData: string;
-};
-
-type EventLog = Log & { args: any };
+import type { VerificationCompletedEventArgs, EventLog } from "@/types";
 
 export default function VerificationComponent() {
   const account = useAccount();
   const config = useConfig();
+  const router = useRouter();
 
   const [selfApp, setSelfApp] = useState<SelfApp>();
   const [universalLink, setUniversalLink] = useState("");
@@ -144,12 +136,16 @@ export default function VerificationComponent() {
 
         if (!res.ok) throw new Error("API request failed");
 
-        console.log("result:", await res.json());
+        localStorage.setItem(
+          "verificationResult",
+          JSON.stringify(await res.json()),
+        );
+        router.push("/result");
       } catch (err) {
         console.error("Error calling verify-successful API:", err);
       }
     })();
-  }, [isFNCallback, evReceived]);
+  }, [isFNCallback, evReceived, router]);
 
   return (
     <>
